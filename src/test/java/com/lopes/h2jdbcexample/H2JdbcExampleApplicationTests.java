b1a950df7d1e47424aa324510dea4d587ba2c746
Anderson Lopes <anderson.lopes@gmail.com>
2019-12-17 22:48:41 +0000

    Include rum application.

---
+++ src/test/java/com/lopes/h2jdbcexample/H2JdbcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopes.h2jdbcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2JdbcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
